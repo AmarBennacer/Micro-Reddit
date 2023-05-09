ed38d7873ee51b375f417c0849345428395a7e06
BENNACER <aminebennacer@gmail.com>
2023-05-09 21:25:14 +0100

    added users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @users = User.all
+  end
+
   def show
-    @user = current_user
-   # @posts = @user.posts
+     @user = User.find(params[:id])
 
     #debugger
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
